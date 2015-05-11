4a4700f653b2cab673fc3ef2cd919597f6a193f6
Micah Zakahi <miczak33@gmail.com>
2015-05-11 12:22:30 -0700

    bit of refactor

--- runit.sh
+++ runit.sh
@@ -3,8 +3,7 @@ link_target=$(echo $curl_target | awk -F/ '{print $3}')
 linked=$(printenv | echo $(grep "\<$link_target_PORT_.*_TCP_PORT\>"))
 if [ ! -z "$linked" ]; then
     link_port=$(echo $linked | cut -d'=' -f2)
-    replace_target="$link_target:$link_port"
-    curl_target=$(echo "$curl_target" | awk -F/ -v var="$replace_target" 'gsub($3,var,$0)')
+    curl_target=$(echo "$curl_target" | awk -F/ -v var="$link_target:$link_port" 'gsub($3,var,$0)')
 fi
 echo "$cron_interval /config/cronjob" > /mycron
 echo "curl -f $curl_target" > /config/cronjob && chmod a+x /config/cronjob
