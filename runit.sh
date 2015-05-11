79ed4e8d756fb6c95b874dd2a624cd2bbef7875f
Micah Zakahi <miczak33@gmail.com>
2015-05-11 12:18:55 -0700

    remove dup line

--- runit.sh
+++ runit.sh
@@ -5,7 +5,6 @@ if [ ! -z "$linked" ]; then
     link_port=$(echo $linked | cut -d'=' -f2)
     replace_target="$link_target:$link_port"
     curl_target=$(echo "$curl_target" | awk -F/ -v var="$replace_target" 'gsub($3,var,$0)')
-    curl_target=$(echo "$curl_target" | awk -F/ -v var="$replace_target" 'gsub($3,var,$0)')
 fi
 echo "$cron_interval /config/cronjob" > /mycron
 echo "curl -f $curl_target" > /config/cronjob && chmod a+x /config/cronjob
