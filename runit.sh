8fdbc91d1a0892930f0758b34a6c3c9a76fdf857
Micah Zakahi <miczak33@gmail.com>
2015-08-12 14:27:06 -0700

    add silent option

--- runit.sh
+++ runit.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 link_target=$(echo $curl_target | awk -F/ '{print $3}')
 linked=$(printenv | echo $(grep "\<$link_target_PORT_.*_TCP_PORT\>"))
+curl_options="-f"
 if [ ! -z "$linked" ]; then
     link_port=$(echo $linked | cut -d'=' -f2)
     curl_target=$(echo "$curl_target" | awk -F/ -v var="$link_target:$link_port" 'gsub($3,var,$0)')
 fi
 echo "$cron_interval /config/cronjob" > /mycron
-echo "curl -f $curl_target" > /config/cronjob && chmod a+x /config/cronjob
+if [ ! -z "$silent" ] && [ "$silent" == "true" ]; then
+	curl_options="${curl_options}s"
+fi
+echo "curl $curl_options $curl_target" > /config/cronjob && chmod a+x /config/cronjob
 crontab /mycron
 crond -f
